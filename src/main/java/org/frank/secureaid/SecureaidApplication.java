9f696ebd82385e493590efdb99a01df829423d19
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-07-08 14:36:26 +0000

    start building - web security config

--- src/main/java/org/frank/secureaid/SecureaidApplication.java
+++ src/main/java/org/frank/secureaid/SecureaidApplication.java
@@ -2,6 +2,8 @@ package org.frank.secureaid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SecureaidApplication {
@@ -10,4 +12,13 @@ public class SecureaidApplication {
         SpringApplication.run(SecureaidApplication.class, args);
     }
 
+}
+
+@RestController
+class AppTestController{
+
+    @GetMapping("/")
+    public String test(){
+        return "Hello World";
+    }
 }
---
+++ src/main/java/org/frank/secureaid/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package org.frank.secureaid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/auth/login","/api/appusers","/api/auth/register", "/test").permitAll()
+                        .anyRequest().authenticated()
+                ).sessionManagement(
+                        session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        // TODO: Add JWT filter later when implementing JWT Logic
+
+
+
+        return http.build();
+
+    }
+}
