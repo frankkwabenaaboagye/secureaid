be94017b77618f3ce9d6cb60525712837e161140
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-07-08 11:16:18 +0000

    initial set up for secure aid

---
+++ src/main/java/org/frank/secureaid/SecureaidApplication.java
@@ -0,0 +1,13 @@
+package org.frank.secureaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureaidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureaidApplication.class, args);
+    }
+
+}
